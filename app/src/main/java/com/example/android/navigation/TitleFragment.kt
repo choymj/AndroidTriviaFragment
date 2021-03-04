f63d7834f09b19823d000f410ac7cf27f61a3d77
choymj <choymj-wm19@student.tarc.edu.my>
2021-03-04 23:53:20 +0800

    init

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,24 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+    class TitleFragment : Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+            val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+                    R.layout.fragment_title,container,false)
+            return binding.root
+        }
+
+
+    }
